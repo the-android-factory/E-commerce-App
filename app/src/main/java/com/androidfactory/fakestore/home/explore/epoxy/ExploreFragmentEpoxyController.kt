188c03f4dc55d555cef7c1b4201dfdb260d4f1f7
developer.dmp <developer.dmp@gmail.com>
2022-12-14 20:21:14 -0500

    Little cleanup

--- app/src/main/java/com/androidfactory/fakestore/home/explore/epoxy/ExploreFragmentEpoxyController.kt
+++ app/src/main/java/com/androidfactory/fakestore/home/explore/epoxy/ExploreFragmentEpoxyController.kt
@@ -11,44 +11,50 @@ class ExploreFragmentEpoxyController(
 ) : TypedEpoxyController<ExploreFragmentViewState>() {
 
     override fun buildModels(data: ExploreFragmentViewState) = when (data) {
-        ExploreFragmentViewState.Loading -> {
-            // Implement loading state
-        }
-        is ExploreFragmentViewState.Error -> {
-            // Implement your error here
-        }
-        is ExploreFragmentViewState.Success -> {
-            HeaderImageEpoxyModel(data.selectedUiProduct.product.image)
-                .id("header_image")
-                .addTo(this)
+        ExploreFragmentViewState.Loading -> setupLoadingState()
+        is ExploreFragmentViewState.Error -> setupErrorState(data)
+        is ExploreFragmentViewState.Success -> setupSuccessState(data)
+    }
 
-            val circleImageEpoxyModels: List<EpoxyModel<*>> = data.allUiProducts.map { uiProduct ->
-                CircleImageEpoxyModel(
-                    imageUrl = uiProduct.product.image,
-                    isSelected = uiProduct.product.id == data.selectedUiProduct.product.id,
-                    onClick = { actions.onProductSelected(uiProduct.product.id) }
-                ).id("image_${uiProduct.product.id}")
+    private fun setupLoadingState() {
+        // Implement loading state
+    }
+
+    private fun setupErrorState(state: ExploreFragmentViewState.Error) {
+        // Implement your error here
+    }
+
+    private fun setupSuccessState(state: ExploreFragmentViewState.Success) {
+        HeaderImageEpoxyModel(state.selectedUiProduct.product.image)
+            .id("header_image")
+            .addTo(this)
+
+        val circleImageEpoxyModels: List<EpoxyModel<*>> = state.allUiProducts.map { uiProduct ->
+            CircleImageEpoxyModel(
+                imageUrl = uiProduct.product.image,
+                isSelected = uiProduct.product.id == state.selectedUiProduct.product.id,
+                onClick = { actions.onProductSelected(uiProduct.product.id) }
+            ).id("image_${uiProduct.product.id}")
+        }
+
+        CarouselModel_().models(circleImageEpoxyModels).id("carousel").addTo(this)
+
+        ProductHeaderDescriptionEpoxyModel(state.selectedUiProduct)
+            .id(state.selectedUiProduct.product.id)
+            .addTo(this)
+
+        ProductFooterEpoxyModel(
+            quantity = state.quantity,
+            uiProduct = state.selectedUiProduct,
+            addToCart = {
+                actions.onAddToCart(productId = state.selectedUiProduct.product.id)
+            },
+            onQuantityUpdate = { newQuantity ->
+                actions.onQuantityChanged(
+                    productId = state.selectedUiProduct.product.id,
+                    quantity = newQuantity
+                )
             }
-
-            CarouselModel_().models(circleImageEpoxyModels).id("carousel").addTo(this)
-
-            ProductHeaderDescriptionEpoxyModel(data.selectedUiProduct)
-                .id(data.selectedUiProduct.product.id)
-                .addTo(this)
-
-            ProductFooterEpoxyModel(
-                quantity = data.quantity,
-                uiProduct = data.selectedUiProduct,
-                addToCart = {
-                    actions.onAddToCart(productId = data.selectedUiProduct.product.id)
-                },
-                onQuantityUpdate = { newQuantity ->
-                    actions.onQuantityChanged(
-                        productId = data.selectedUiProduct.product.id,
-                        quantity = newQuantity
-                    )
-                }
-            ).id("footer_${data.selectedUiProduct.product.id}").addTo(this)
-        }
+        ).id("footer_${state.selectedUiProduct.product.id}").addTo(this)
     }
 }
